f0aeb7cfb42e4f834ac0a4dc3d14ad628d858320
goggalore <junec177@gmail.com>
2018-06-17 15:20:24 -0600

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const rollup = require('rollup-stream');
+const source = require('vinyl-source-stream');
+
+gulp.task('rollup', () => {
+    return rollup({
+        input: 'src/js/port.js',
+        format: 'iife',
+    })
+    .pipe(source('port.js'))
+    .pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', gulp.series('rollup'), () => {
+    gulp.watch(['./src/js/port']);
+});
