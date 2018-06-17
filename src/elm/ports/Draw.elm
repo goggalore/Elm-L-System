5f80ee742dd4e0d1a5c9a24b48226c5de7256f55
goggalore <junec177@gmail.com>
2018-06-17 15:24:59 -0600

    remove commented out code from ports/Draw.elm

--- src/elm/ports/Draw.elm
+++ src/elm/ports/Draw.elm
@@ -3,22 +3,4 @@ port module Ports.Draw exposing (draw)
 import Models exposing (Model)
 
 
--- type alias Data =
---     { commands : String
---     , angle : Float
---     , orientation : Float
---     , animate : Bool
---     }
-
-
 port draw : Model -> Cmd msg
-
-
-
--- data : Model -> Data
--- data model =
---     { commands = getCommandString model
---     , angle = model.angle
---     , orientation = model.orientation
---     , animate = False
---     }
