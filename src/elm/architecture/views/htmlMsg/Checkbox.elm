90644c7d2af565f48cb80effe824fb404e0d1f62
goggalore <junec177@gmail.com>
2018-07-02 17:40:05 -0600

    update Checkbox.elm to have checkbox ticked by default

--- src/elm/architecture/views/htmlMsg/Checkbox.elm
+++ src/elm/architecture/views/htmlMsg/Checkbox.elm
@@ -1,13 +1,13 @@
 module HtmlMsg.Checkbox exposing (checkbox)
 
 import Html exposing (Html, label, input, text)
-import Html.Attributes exposing (type_)
+import Html.Attributes exposing (checked, type_)
 import Html.Events exposing (onClick)
 
 
 checkbox : msg -> String -> Html msg
 checkbox msg name =
     label []
-        [ input [ type_ "checkbox", onClick msg ] []
+        [ input [ type_ "checkbox", checked True, onClick msg ] []
         , text name
         ]
